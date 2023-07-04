3c4c25e4346430c93289afaca6e0e765886c3656
ozansert <ozansert1993@gmail.com>
2023-07-03 22:33:22 -0400

    Refactor attribute reference

--- JooleGroupProject.DAL/Interfaces/IAttributeRepo.cs
+++ JooleGroupProject.DAL/Interfaces/IAttributeRepo.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Attribute = JooleGroupProject.DAL.Models.Attribute;
 
 namespace JooleGroupProject.DAL.Interfaces
 {
     public interface IAttributeRepo 
     {
-        JooleGroupProject.DAL.Models.Attribute GetAttribute(int id);
-        IEnumerable<JooleGroupProject.DAL.Models.Attribute> GetAttributes();
-        IEnumerable<JooleGroupProject.DAL.Models.Attribute> GetAttributesByTechSpec();
-        IEnumerable<JooleGroupProject.DAL.Models.Attribute> GetAttributesByType();
+        Attribute GetAttribute(int id);
+        IEnumerable<Attribute> GetAttributes();
+        IEnumerable<Attribute> GetAttributesByTechSpec();
+        IEnumerable<Attribute> GetAttributesByType();
     }
 }
--- JooleGroupProject.RepositoryLayer/Repositories/AttributeRepo.cs
+++ JooleGroupProject.RepositoryLayer/Repositories/AttributeRepo.cs
@@ -5,30 +5,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using JooleGroupProject.DAL.Models;
+using Attribute = JooleGroupProject.DAL.Models.Attribute;
 
 namespace JooleGroupProject.RepositoryLayer.Repositories
 {
-    public class AttributeRepo : GenericRepo<JooleGroupProject.DAL.Models.Attribute>, IAttributeRepo
+    public class AttributeRepo : GenericRepo<Attribute>, IAttributeRepo
     {
         public AttributeRepo(MyDBContext _dbcontext) : base(_dbcontext)
         {
 
         }
-        public JooleGroupProject.DAL.Models.Attribute GetAttribute(int id)
+        public Attribute GetAttribute(int id)
         {
             return this.GetByID(id);
         }
 
-        public IEnumerable<JooleGroupProject.DAL.Models.Attribute> GetAttributes()
+        public IEnumerable<Attribute> GetAttributes()
         {
             return this.GetAll();
         }
         
-        public IEnumerable<JooleGroupProject.DAL.Models.Attribute> GetAttributesByTechSpec()
+        public IEnumerable<Attribute> GetAttributesByTechSpec()
         {
             return this.GetMany(x => x.IsTechSpec == true);
         }
-        public IEnumerable<JooleGroupProject.DAL.Models.Attribute> GetAttributesByType()
+        public IEnumerable<Attribute> GetAttributesByType()
         {
             return this.GetMany(x => x.IsType == true);
         }
