a73cb5d92f6ac5ac6637134802dad62510ebdb3b
ozansert <ozansert1993@gmail.com>
2023-07-04 00:49:48 -0400

    Add Mock Category for seed method

---
+++ JooleGroupProject.RepositoryLayer/Migrations/202307040438344_AddCategory.cs
@@ -0,0 +1,18 @@
+﻿namespace JooleGroupProject.RepositoryLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCategory : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Product", "ModelYear", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Product", "ModelYear");
+        }
+    }
+}
--- JooleGroupProject.RepositoryLayer/Migrations/Configuration.cs
+++ JooleGroupProject.RepositoryLayer/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 ﻿namespace JooleGroupProject.RepositoryLayer.Migrations
 {
+    using JooleGroupProject.DAL.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -18,6 +20,21 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+
+            var categories = new List<Category>
+            {
+                new Category { CategoryID = 1, CategoryName = "Mechanical" },
+                new Category { CategoryID = 2, CategoryName = "Electrical" },
+                new Category { CategoryID = 3, CategoryName = "Stationary" },
+                new Category { CategoryID = 4, CategoryName = "Furniture" },
+            };
+
+            foreach (var category in categories)
+            {
+                context.Categories.AddOrUpdate(c => c.CategoryID, category);
+            }
+
+            context.SaveChanges();
         }
     }
 }
