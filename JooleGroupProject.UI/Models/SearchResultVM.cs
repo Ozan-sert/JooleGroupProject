20f0c892a0fefec7bcdd08007f86d70c5e4a6d5f
hoyatsui <hoya.hyx@gmail.com>
2023-08-06 01:55:53 -0400

    init test

---
+++ JooleGroupProject.UI/Models/SearchResultVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JooleGroupProject.ServiceLayer;
+using JooleGroupProject.ServiceLayer.Models;
+using JooleGroupProject.ServiceLayer.Services;
+
+namespace JooleGroupProject.UI.Models
+{
+    public class SearchResultVM
+    {
+        public int SubCategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string SubCategoryName { get; set; }
+
+        public int Year1 { get; set; }
+        public int Year2 { get; set; }
+
+        public List<TechSpecFilterDTO> techSpecFilters { get; set; }
+        public IEnumerable<ProductDTO> Products { get; set; }
+
+    }
+}
