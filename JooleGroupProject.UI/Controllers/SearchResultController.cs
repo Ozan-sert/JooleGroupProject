54895c1c14a2e5df5cc1d4ae90c08fee09f56af6
hoyatsui <hoya.hyx@gmail.com>
2023-08-07 01:26:16 -0400

    feat(UI): Add SearchResult Controller and its views(filter in product summary page & test view for resultservice)

--- JooleGroupProject.UI/Controllers/SearchResultController.cs
+++ JooleGroupProject.UI/Controllers/SearchResultController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using JooleGroupProject.ServiceLayer.Interfaces;
+using JooleGroupProject.ServiceLayer.Models;
+using JooleGroupProject.ServiceLayer.Services;
+using JooleGroupProject.UI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +12,41 @@ namespace JooleGroupProject.UI.Controllers
 {
     public class SearchResultController : Controller
     {
+        // Instantiate the service layer
+        private readonly ResultService resultService = new ResultService();
+        private readonly SearchService searchService = new SearchService();
+
+
         // GET: SearchResult
-        public ActionResult Index()
+        public ActionResult Index(int subCategoryID, string categoryName, string subCategoryName)
         {
-            return View();
+            SearchResultVM viewModel = new SearchResultVM();
+            viewModel.techSpecFilters = resultService.GetTechSpecFilterNamesForSubCategory(subCategoryID);
+            viewModel.CategoryName = categoryName;
+            viewModel.SubCategoryName = subCategoryName;
+            return View(viewModel);
+        }
+
+
+        // Test Services:
+        public ActionResult TechSpecFilters(int subCategoryID)
+        {
+            SearchResultVM viewModel = new SearchResultVM();
+            viewModel.techSpecFilters = resultService.GetTechSpecFilterNamesForSubCategory(subCategoryID);
+            return View(viewModel.techSpecFilters);
+        }
+
+        public ActionResult ProducsInSubcategory(int subCategoryID)
+        {
+            SearchResultVM viewModel = new SearchResultVM();
+            viewModel.Products = resultService.GetProductsBySubCategory(subCategoryID);
+            return View(viewModel.Products);
+        }
+        public ActionResult ProductsFilteredByModelYear(int sub, int year1, int year2)
+        {
+            SearchResultVM viewModel = new SearchResultVM();
+            viewModel.Products = resultService.GetProductsFiltered(sub, year1, year2);
+            return View(viewModel.Products);
         }
     }
 }
